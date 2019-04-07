4677fa48deb4630ef39676b25ba2b9e646928501
starcloudxxx <hzxstarcloud@hotmail.com>
2019-04-07 16:44:04 +0800

    所有文件

---
+++ universalspider/config_INFO.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   config_INFO.py
+@Time    :   2019/03/31 21:42:50
+@Author  :   Hzx 
+@Version :   1.0
+@Contact :   hzxstarcloud@hotmail.com
+@Desc    :   None
+'''
+
+# here put the import lib
+
+CONFIG_HOST	="localhost"
+CONFIG_DB	="tempnews"
+CONFIG_PORT	=3306
+CONFIG_PSWD ="8512"
+CONFIG_TABLE="news"
+CONFIG_USER	="user"
+
+DATA_DB		="tempnews"
+DATA_HOST	="localhost"
+DATA_PORT	=3306
+DATA_PSWD	="8512"
+DATA_USER	="user"
+
+
+SQL_DB		="none"
+SQL_HOST	=None
+SQL_PSWD	=None
+SQL_USER	=None
